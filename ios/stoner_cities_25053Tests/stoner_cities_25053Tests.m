dcc2718aa93434e7c540edf2f63b8d30958fda07
Crowdbotics <team@crowdbotics.com>
2021-03-16 17:00:51 +0000

    Initial commit.

---
+++ ios/stoner_cities_25053Tests/stoner_cities_25053Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface stoner_cities_25053 : XCTestCase
+
+@end
+
+@implementation stoner_cities_25053
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://stoner-cities-25053.botics.co",
+  defaultTimeout: 5000
+};
