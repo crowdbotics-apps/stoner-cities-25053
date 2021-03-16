46955f0831d866eb551b7efb12feb3e8f00549aa
Crowdbotics <team@crowdbotics.com>
2021-03-16 17:06:43 +0000

    Update models

--- backend/stoner_cities_25053/wsgi.py
+++ backend/stoner_cities_25053/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stoner_cities_25053.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stoner_cities_25053.settings")
 
 application = get_wsgi_application()
